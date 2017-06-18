df46ea6b08fee5c139910ee7e5f6bb624a559fe0
Ronald Moesbergen <rmoesbergen@asp4all.nl>
2017-06-18 11:25:33 +0200

    Add 2 fields to excel export

--- LedenAdministratie/LedenAdministratie/views.py
+++ LedenAdministratie/LedenAdministratie/views.py
@@ -163,11 +163,12 @@ def do_export(request, speltak):
     writer.writerow(
         ['Voornaam', 'Achternaam', 'SN', 'Geb. Datum', 'Leeftijd', 'Geslacht', 'Speltak', 'E-mail', 'Straat',
          'Postcode', 'Woonplaats', 'Telnr', 'Mobiel', 'Mobiel Ouder 1', 'Mobiel Ouder 2',
-         'E-mail Ouder 1', 'E-mail Ouder 2', 'FotoPubliek'])
+         'E-mail Ouder 1', 'E-mail Ouder 2', 'FotoPubliek', 'Inschrijf Datum', 'Jub. Badge'])
 
     for lid in leden:
         writer.writerow([lid.first_name, lid.last_name, lid.scouting_nr, lid.gebdat, lid.age, lid.geslacht, lid.speltak,
                          lid.email_address, lid.straat, lid.postcode, lid.woonplaats, lid.telnr,
-                         lid.mobiel, lid.mobiel_ouder1, lid.mobiel_ouder2, lid.email_ouder1, lid.email_ouder2, lid.foto])
+                         lid.mobiel, lid.mobiel_ouder1, lid.mobiel_ouder2, lid.email_ouder1, lid.email_ouder2, lid.foto,
+                         lid.inschrijf_datum_sn, lid.jub_badge])
 
     return response
