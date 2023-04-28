05c05ad939fb388a62042467f58818de8ae686cc
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2023-04-28 11:39:20 +0200

    chore: dependency updates fix: automatically retry deferred emails

--- deploy/jobs.sh
+++ deploy/jobs.sh
@@ -7,9 +7,12 @@ while true; do
   python manage.py clearsessions
   echo "[$(date)] Cleaning old oauth tokens."
   python manage.py cleartokens
+  echo "[$(date)] Retrying deferred emails."
+  python manage.py retry_deferred --cron 1
   echo "[$(date)] Cleaning old email logs."
   python manage.py purge_mail_log -r all 90
   echo "{$(date)] Syncing Stripcards."
+
   python manage.py sync_stripcards
   sleep 3600
 done
