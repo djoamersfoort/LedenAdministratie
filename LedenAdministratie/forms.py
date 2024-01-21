862beeee30eea0bf6ee2ee49c9f9154ec7766ae1
sverben <59171289+sverben@users.noreply.github.com>
2024-01-21 11:51:12 +0100

    Feat: Add support for notifications

--- LedenAdministratie/forms.py
+++ LedenAdministratie/forms.py
@@ -100,6 +100,7 @@ class EmailSendForm(forms.Form):
         choices=VALID_RECIPIENTS, widget=forms.CheckboxSelectMultiple
     )
     subject = forms.CharField(max_length=255)
+    description = forms.CharField(max_length=255)
     body = forms.CharField(widget=TinyMCE(mce_attrs={"cols": 80, "height": 500}))
     attachment = forms.FileField(required=False)
 
